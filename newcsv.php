ab4555af7cc0ad0d56c3144e2be2130cd40d0087
kofi osei-tutu <ko45@afsconnect1.njit.edu>
2014-10-03 13:00:40 -0400

    made colleges links

--- newcsv.php
+++ newcsv.php
@@ -1,9 +1,9 @@
 <?php
-
+//reading the file
 $first_row = TRUE;
 ini_set('auto_detect_line_endings',TRUE);
 if (($handle = fopen("hd2013.csv", "r")) !== FALSE) { //opens and reads the file
-    while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    while (($row = fgetcsv($handle, 5000, ",")) !== FALSE) {
      if($first_row == TRUE) {
        $column_heading = $row;
        $first_row = FALSE;
@@ -16,21 +16,25 @@ if (($handle = fopen("hd2013.csv", "r")) !== FALSE) { //opens and reads the file
 
     fclose($handle);
 }
-
-//print_r($records);
+//for each record, display college name
+foreach ($records as $record){
+		$colleges[] =  $record['UNITID'] .'-' .$record['INSTNM'] . '</a>';
+		
+	}
+	
+//print_r($colleges);// prints out the college names into arrays
 
 if(empty($_GET)) {
 	
     foreach($records as $record) {
-      $i++;
-      $record_num = $i - 1;
-      echo '<a href=' . '"http://web.njit.edu/~ko45/is218/' . $record_num . '"' . '>College Name ' . $i . ' </a>';
+    	$i++;
+     echo  '<a href = '.' "http://web.njit.edu/~ko45/is218/newcsv.php?record=' . $i . '"'. '>'.$record['INSTNM']. '</a>';
 		
       echo '</p>';
     }
   }
 
-  $record = $records[$_GET['record']];
+  /*$record = $records[$_GET['record']];
   
    foreach($record as $key => $value) {
     echo $key . ': ' . $value . "<br>\n";
